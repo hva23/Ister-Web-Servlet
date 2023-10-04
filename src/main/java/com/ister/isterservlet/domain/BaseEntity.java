455d49a77984f6d716d599eeb6aaadf67a487855
hva23 <haskihossein@gmail.com>
2023-10-04 15:39:50 +0330

    Add to project

---
+++ src/main/java/com/ister/isterservlet/domain/BaseEntity.java
@@ -0,0 +1,34 @@
+package com.ister.isterservlet.domain;
+
+import java.util.Date;
+
+public class BaseEntity<PK> {
+
+    private PK id;
+    private String createdDate;
+    private String lastModifiedDate;
+
+    public PK getId() {
+        return id;
+    }
+
+    public void setId(PK id) {
+        this.id = id;
+    }
+
+    public String getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(String createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public String getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    public void setLastModifiedDate(String lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
+}
